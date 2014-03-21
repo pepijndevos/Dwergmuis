6f9e39eada21555ded80c0b17164fd043432cc4f
Pepijn de Vos <pepijndevos@gmail.com>
2014-03-21 14:05:17 +0100

    Implement clicking
    Uses a double-throw switch.
    No debouncing is needed.
    ANY raising edge on the one pin means a press.
    ANY falling edge on the other pin means a release.

--- dwergmuis.ino
+++ dwergmuis.ino
@@ -1,13 +1,28 @@
+enum button_state {
+   BUTTON_RELEASED     = 0,
+   BUTTON_ACK_RELEASED = 1,
+   BUTTON_PRESSED      = 2,
+   BUTTON_ACK_PRESSED  = 3
+};
+
 volatile int scroll_steps = 0;
+volatile enum button_state left_state = BUTTON_ACK_RELEASED;
 
 void setup() {
+
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  attachInterrupt(11, left_press, RISING);
+  attachInterrupt(12, left_release, FALLING);
+  
+  // rotary encoder
   pinMode(16, INPUT_PULLUP);
   pinMode(17, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
-  
   attachInterrupt(16, read_encoder, CHANGE);
   attachInterrupt(17, read_encoder, CHANGE);
   
+  pinMode(13, OUTPUT);       // LED
+
   Serial.begin(9600);
 }
 
@@ -15,6 +30,10 @@ void loop() {
   if (scroll_steps) {
     Mouse.scroll(scroll_steps);
     scroll_steps = 0;
+  }
+  if (!(left_state & 1)) { // not ACK
+    left_state = (enum button_state)((int)left_state | 1); // ACK
+     Mouse.set_buttons(((left_state & 2) >> 1), 0, 0);
   }
 }
 
@@ -27,4 +46,16 @@ void read_encoder()
   old_AB <<= 2;                   //remember previous state
   old_AB |= ( GPIOB_PDIR & 0x03 );  //add current state
   scroll_steps += ( enc_states[( old_AB & 0x0f )]);
+}
+
+void left_press() {
+  if (left_state == BUTTON_ACK_RELEASED) {
+    left_state = BUTTON_PRESSED;
+  }
+}
+
+void left_release() {
+  if (left_state == BUTTON_ACK_PRESSED) {
+    left_state = BUTTON_RELEASED;
+  }
 }
